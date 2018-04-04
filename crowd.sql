b3314c7ada72c181ddfad47b694666fc26d91468
Ryan <datmanwich@gmail.com>
2018-04-04 16:11:46 -0700

    Added the sql and the index.php files

---
+++ crowd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Crowd
+(
+    id              INT(6)      UNSIGNED AUTO_INCREMENT PRIMARY KEY
+    ,title          VARCHAR(30) NOT NULL
+    ,optionOne      VARCHAR(30) NOT NULL
+    ,optionTwo      VARCHAR(30) NOT NULL
+    ,optionThree    VARCHAR(30) NOT NULL
+    ,optionFour     VARCHAR(30) NOT NULL
+);
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Crowd Vote</h2>
+
+<form action=" ">
+    Title:<br>
+    <input type="text" name="title">
+    <br>
+    Option 1:<br>
+    <input type="text" name="option1">
+    <br>
+    Option 2:<br>
+    <input type="text" name="option2">
+    <br>
+    Option 3:<br>
+    <input type="text" name="option3">
+    <br>
+    Option 4:<br>
+    <input type="text" name="option4">
+    <br><br>
+    <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
