cffe728f2b38a81c40621f3907b5bfd2a21dd8ec
Ryan <datmanwich@gmail.com>
2018-06-25 14:27:04 -0700

    Added css folder and updated input form with bootstrap

--- index.php
+++ index.php
@@ -11,17 +11,22 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1">
 
+        <!-- CSS Files -->
+        <link rel="stylesheet" href="css/base.css" type="text/css" title="Default">
+
         <title>Crowd Vote</title>
 
         <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-                                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"     
-                                crossorigin="anonymous">
+        <link   rel="stylesheet"  
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"     
+                crossorigin="anonymous">
 
         <!-- Optional theme -->
-        <link rel="stylesheet"  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
-                                integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" 
-                                crossorigin="anonymous">
+        <link   rel="stylesheet"  
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
+                integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" 
+                crossorigin="anonymous">
 
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
@@ -31,32 +36,42 @@
         <!-- Crowd Vote form validation -->
         <script type="text/javascript" src="formValidation.js"></script>
     </head>
-    
 <body lang="en">
 
     <h1>Crowd Vote</h1>
 
-    <form   name="crowdForm" 
-            action="crowdScript.php"   
-            method="post" 
-            onsubmit="return validateForm()">
-        Title:<br>
-        <input type="text" name="title">
-        <br>
-        Option 1:<br>
-        <input type="text" name="option1">
-        <br>
-        Option 2:<br>
-        <input type="text" name="option2">
-        <br>
-        Option 3:<br>
-        <input type="text" name="option3">
-        <br>
-        Option 4:<br>
-        <input type="text" name="option4">
-        <br><br>
-        <input type="submit" name="submit" value="Submit">
-    </form> 
+    <div class="container-fluid">
+        <div class="formInput row">
+            <form   name="crowdForm" 
+                    class="form-inline"
+                    action="crowdScript.php"   
+                    method="post" 
+                    onsubmit="return validateForm()">
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="title" class="form-control" id="title">
+                </div>
+                <div class="form-group">
+                    <label for="option1">Option 1:</label>
+                    <input type="option1" class="form-control" id="option1">
+                </div>
+                <div class="form-group">
+                    <label for="option2">Option 2:</label>
+                    <input type="option2" class="form-control" id="option2">
+                </div>
+                <div class="form-group">
+                    <label for="option3">Option 3:</label>
+                    <input type="option3" class="form-control" id="option3">
+                </div>
+                <div class="form-group">
+                    <label for="option4">Option 4:</label>
+                    <input type="option4" class="form-control" id="option4">
+                </div>
+                <br><br>
+                <input type="submit" class="btn btn-default" name="submit" value="Submit">
+            </form> 
+        </div>
+    </div>
     <br><br> 
     
     <a href="user.php">Random Choice</a>
