6b720fe23415b50f11bba379cab186c54d3b0205
Ryan <datmanwich@gmail.com>
2018-06-30 22:29:52 -0700

    Fixed the userChoice error where we couldnt access the page

--- user.php
+++ user.php
@@ -9,6 +9,7 @@
     session_start();
     require_once('mysqli_connect.php');
     include 'functions.php';
+    include 'userScript.php';
     ob_start();
 
     $list = connect();
@@ -124,43 +125,3 @@
 </div>
 </body>
 </html>
-
-<?
-if(isset($_GET['submit'])) {
-
-if (!isset($_SESSION["choice"])) {
-        $_SESSION["choice"] = $_GET['choice'];
-        $choice = $_SESSION["choice"];
-}
-
-if($_SESSION["choice"] == $_SESSION["one"]){
-    $picked = "oneVal";
-    if (!isset($_SESSION["option"])) {
-        $_SESSION["option"] = $picked;
-    } 
-} 
-
-if($_SESSION["choice"] == $_SESSION["two"]){
-    $picked = "twoVal";
-    if (!isset($_SESSION["option"])) {
-        $_SESSION["option"] = $picked;
-    } 
-} 
-
-if($_SESSION["choice"] == $_SESSION["three"]){
-    $picked = "threeVal";
-    if (!isset($_SESSION["option"])) {
-        $_SESSION["option"] = $picked;
-    } 
-} 
-
-if($_SESSION["choice"] == $_SESSION["four"]){
-    $picked = "fourVal";
-    if (!isset($_SESSION["option"])) {
-        $_SESSION["option"] = $picked;
-    } 
-} 
-header("Location: userChoice.php"); 
-}
-ob_end_flush();
-?>
